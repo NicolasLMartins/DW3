967c3a68d5d7c55f1eeb5de1997cdeb7a3191408
Colégio Concórdia <vs2020concordia@hotmail.com>
2023-10-05 17:02:22 -0300

    Sem Tempo Irmao

--- Projeto01-0921/app/Controllers/Produtos.php
+++ Projeto01-0921/app/Controllers/Produtos.php
@@ -2,6 +2,7 @@
 
 namespace App\Controllers;
 
+use App\Models\ProdutoModel;
 use App\Controllers\BaseController;
 
 class Produtos extends BaseController
@@ -9,12 +10,79 @@ class Produtos extends BaseController
     public function index()
     {
         $titulo = "Meus Produtos";
+
+        $produtos = [
+            [
+                'nome' => "Notebook",
+                'descricao' => "Produto Usado",
+                'valor_de_compra' => 899.99,
+                'valor_de_venda' => 1399.9,
+                'quantidade' => 11,
+                'validade' => "",
+            ],
+            [
+                'nome' => "Computador I5",
+                'descricao' => "Produto Novo",
+                'valor_de_compra' => 1899.99,
+                'valor_de_venda' => 2399.9,
+                'quantidade' => 1,
+                'validade' => "",
+            ],
+        ];
+
         $data['titulo'] = $titulo;
+        $data['produtos'] = $produtos;
+
         echo view('produtos/index', $data);
     }
 
     public function novo()
     {
         return "Novo produto";
+    }
+
+    public function model()
+    {
+        $produto_model = new ProdutoModel();
+
+        // $produtos = $produto_model->findAll();
+
+        // dd($produtos);
+
+        // $produtos =  $produto_model->where('id_produto', 1)->first();
+
+        // dd($produtos);
+
+        // $produto_model->insert([
+        //     'nome' => "Produto Teste",
+        //     'descricao' => "Produto Novo",
+        //     'valor_de_compra' => 199,
+        //     'valor_de_venda' => 300,
+        //     'quantidade' => 10,
+        //     'validade' => "",
+        // ]);
+
+        // dd();
+
+        // $dados = [
+        //     'nome' => "Produto Motorola",
+        //     'descricao' => "Produto Usado",
+        //     'valor_de_compra' => 1990,
+        //     'valor_de_venda' => 3000,
+        //     'quantidade' => 12,
+        //     'validade' => ""
+        // ];
+        // $produto_model
+        //     ->where('id_produto', 4)
+        //     ->set($dados)
+        //     ->update();
+        // dd();
+
+        $produto_model
+            ->where('id_produto', 4)
+            ->delete();
+        dd();
+
+        // dd();
     }
 }
