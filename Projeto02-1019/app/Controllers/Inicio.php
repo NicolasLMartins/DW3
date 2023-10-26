45523e13070dda1c9c4d6c5e84edd024879b900c
NicolasLMartins <nicolaslm2006@gmail.com>
2023-10-26 16:19:17 -0300

    ADD echo view to Inicio Controller

--- Projeto02-1019/app/Controllers/Inicio.php
+++ Projeto02-1019/app/Controllers/Inicio.php
@@ -8,6 +8,8 @@ class Inicio extends BaseController
 {
     public function index()
     {
-        //
+        echo view('templates/header');
+        echo view('inicio/index');
+        echo view('templates/footer');
     }
 }
