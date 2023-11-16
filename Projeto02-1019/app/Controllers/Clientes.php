aa554c1bad452bef87f4e679488b3f9cd3e4dd86
NicolasLMartins <nicolaslm2006@gmail.com>
2023-11-16 17:01:22 -0300

    UPDATE tudo (sem tempo irmao)

--- Projeto02-1019/app/Controllers/Clientes.php
+++ Projeto02-1019/app/Controllers/Clientes.php
@@ -36,8 +36,43 @@ class Clientes extends BaseController
         $data = $this->request->getVar();
 
         // dd($data);
+        if (isset($data['id_cliente'])):
+            $this->cliente_model->where('id_cliente', $data['id_cliente'])->set($data)->update();
+
+            return redirect()->to("/clientes/editar/{$data['id_cliente']}");
+        endif;
 
         $this->cliente_model->insert($data);
-        return redirect()->to('clientes/novo');
+        return redirect()->to('/clientes');
+    }
+
+    public function editar($id_cliente)
+    {
+        // echo $id_cliente;
+        $cliente = $this->cliente_model->where('id_cliente', $id_cliente)->first();
+
+        $data['cliente'] = $cliente;
+
+        echo view('templates/header');
+        echo view('clientes/editar', $data);
+        echo view('templates/footer');
+    }
+
+    public function excluir($id_cliente)
+    {
+        $cliente = $this->cliente_model->where('id_cliente', $id_cliente)->delete();
+
+        return redirect()->to('/clientes');
+    }
+
+    public function exibir($id_cliente)
+    {
+        $cliente = $this->cliente_model->where('id_cliente', $id_cliente)->first();
+
+        $data['cliente'] = $cliente;
+
+        echo view('templates/header');
+        echo view('clientes/exibir', $data);
+        echo view('templates/footer');
     }
 }
