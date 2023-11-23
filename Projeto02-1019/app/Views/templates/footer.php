c4cd00322244a16550cb3853a15181b440ec88dc
NicolasLMartins <nicolaslm2006@gmail.com>
2023-11-23 17:10:43 -0300

    UPDATE footer info

--- Projeto02-1019/app/Views/templates/footer.php
+++ Projeto02-1019/app/Views/templates/footer.php
@@ -12,10 +12,10 @@
 <footer class="main-footer">
     <!-- To the right -->
     <div class="float-right d-none d-sm-inline">
-        Anything you want
+        DevOps: Nícolas Lisbôa Martins
     </div>
     <!-- Default to the left -->
-    <strong>Copyright &copy; 2014-2021 <a href="https://adminlte.io">AdminLTE.io</a>.</strong> All rights reserved.
+    <strong>NLMedics &copy; <?= date('Y') ?> <a href="https://www.instagram.com/nicolas_lis_mar/">Nícolas Lisbôa Martins</a>.</strong>Todos os direitos reservados.
 </footer>
 </div>
 <!-- ./wrapper -->
