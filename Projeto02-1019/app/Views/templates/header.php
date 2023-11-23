3f5ef87ef3771f8f68d7042240f53b53205bf0cf
NicolasLMartins <nicolaslm2006@gmail.com>
2023-11-23 17:10:32 -0300

    UPDATE header title and language

--- Projeto02-1019/app/Views/templates/header.php
+++ Projeto02-1019/app/Views/templates/header.php
@@ -3,12 +3,12 @@
 This is a starter template page. Use this page to start your new project from
 scratch. This page gets rid of all links and provides the needed markup only.
 -->
-<html lang="pt_br">
+<html lang="pt-br">
 
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>AdminLTE 3 | Starter</title>
+    <title>NLMedics</title>
 
     <!-- Google Font: Source Sans Pro -->
     <link rel="stylesheet"
