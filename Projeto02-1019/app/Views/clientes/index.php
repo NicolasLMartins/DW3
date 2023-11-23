e2e551c22744d508049b4d755850fe944a95d8ce
NicolasLMartins <nicolaslm2006@gmail.com>
2023-11-23 15:47:59 -0300

    COMMENT pagination in clientes index

--- Projeto02-1019/app/Views/clientes/index.php
+++ Projeto02-1019/app/Views/clientes/index.php
@@ -68,7 +68,7 @@
                             </table>
                         </div>
 
-                        <div class="card-footer clearfix">
+                        <!-- <div class="card-footer clearfix">
                             <ul class="pagination pagination-sm m-0 float-right">
                                 <li class="page-item"><a class="page-link" href="#">«</a></li>
                                 <li class="page-item"><a class="page-link" href="#">1</a></li>
@@ -76,7 +76,7 @@
                                 <li class="page-item"><a class="page-link" href="#">3</a></li>
                                 <li class="page-item"><a class="page-link" href="#">»</a></li>
                             </ul>
-                        </div>
+                        </div> -->
                     </div>
                 </div>
             </div>
