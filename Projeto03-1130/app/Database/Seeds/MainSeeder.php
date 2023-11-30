541b10148b8a79b573975e667ba84114adbd5901
NicolasLMartins <nicolaslm2006@gmail.com>
2023-11-30 16:13:56 -0300

    ADD MainSeeder Seeder

---
+++ Projeto03-1130/app/Database/Seeds/MainSeeder.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Database\Seeds;
+
+use CodeIgniter\Database\Seeder;
+
+class MainSeeder extends Seeder
+{
+    public function run()
+    {
+        for ($i=0; $i < 5; $i++) { 
+            $this->call('ClientesSeeder');
+        }
+    }
+}
